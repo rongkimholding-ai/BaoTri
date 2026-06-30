88c90b41b5066e3737cd510403128ab123d63c6f
phongvh_mpos <phongvh@mpos.vn>
2026-06-30 14:36:02 +0700

    thêm người full view

--- config/special_user.php
+++ config/special_user.php
@@ -3,6 +3,7 @@
 return [
     'full_view' => [
         "liemhoang.support.hcm@tocotocotea.com",
+        "dunguyen.support@tocotocotea.com",
         "ktnb1@tocotocotea.com",
         "ktnb2@tocotocotea.com",
         "loantran@tocotocotea.com",
